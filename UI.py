f387ade8e78dc8322c0c54e50fe32a4168f5deea
Iuliia <iuliiachoporova@gmail.com>
2020-02-26 21:39:43 +0000

    added UI, need to connect it to other modules

---
+++ UI.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+def get_input():
+    keyword = entry.get();
+    print(keyword)
+
+m = tk.Tk()
+m.title('Sentiment Tool')
+tk.Label(m, text='Key Word').grid(row=0)
+entry = tk.Entry(m)
+entry.grid(row=0, column=1)
+tk.Button(m, text='Exit',command=m.quit).grid(row=3,column=0,sticky=tk.W,pady=4)
+tk.Button(m, text='Search', command=get_input).grid(row=3, column=1, sticky=tk.W,pady=4)
+m.mainloop()
